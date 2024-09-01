bef14ae1ce9e3daed8dafb9425f004fe60c508a2
Swayam Arunav Khuntia <khuntiaswayam@gmail.com>
2024-09-02 00:32:49 +0530

    Up commit

--- app/src/main/java/com/krish/jobspot/model/MockDetail.kt
+++
@@ -1,8 +0,0 @@
-package com.krish.jobspot.model
-
-data class MockDetail(
-    var mockId : String = "",
-    var studentCount: String = "0",
-    var mockName: String = "",
-    var studentIds: MutableList<String> = mutableListOf<String>()
-)
